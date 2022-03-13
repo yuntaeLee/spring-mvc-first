ff567b59c601deeb7fdfd42b402cd91b6decdff3
yuntaeLee <leeyt1201@gmail.com>
2022-03-13 21:57:31 +0900

    Use servlet and jsp and mvc

--- servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -1,2 +1,25 @@
-package hello.servlet.web.servletmvc;public class MvcMemberFormServlet {
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
+        /**
+         * forward() : 다른 서블릿이나 JSP 로 이동할 수 있는 기능
+         * 리다이렉트 개념이 아닌 서버 내부에서 다시 호출이 발생함.
+         */
+        dispatcher.forward(request, response);
+    }
 }
