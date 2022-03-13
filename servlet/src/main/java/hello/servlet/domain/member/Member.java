a56a483d2faa385591595359b0257cd895a891db
yuntaeLee <leeyt1201@gmail.com>
2022-03-13 22:03:01 +0900

    Add Member object

--- servlet/src/main/java/hello/servlet/domain/member/Member.java
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -1,2 +1,20 @@
-package hello.servlet.domain.member;public class Member {
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member() {
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
 }
