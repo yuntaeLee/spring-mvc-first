73476df5a99f3c040a04fefa563b1c82b9ee802f
yuntaeLee <leeyt1201@gmail.com>
2022-03-23 17:27:29 +0900

    Create object

---
+++ springmvc/src/main/java/hello/springmvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.springmvc.basic;
+
+import lombok.Data;
+
+/**
+ * @Data -> @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor 자동 적용
+ */
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
