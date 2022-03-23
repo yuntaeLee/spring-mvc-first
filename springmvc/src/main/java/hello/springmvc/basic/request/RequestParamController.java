e72e5afacd6f67c33028c48242c6d95921e029a9
yuntaeLee <leeyt1201@gmail.com>
2022-03-23 17:26:40 +0900

    Get Request Parameter

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,10 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -91,6 +90,29 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "Ok";
+    }
+
+    /**
+     * @ModelAttribute 가 있으면 HelloData 객체를 생성하하고,
+     * 요청 파라미터 이름으로 HelloData 객체의 프로퍼티를 찾는다.
+     * 그리고 해당 프로퍼티의 setter 를 호출해서 파라미터의 값을 바인딩한다.
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "Ok";
+    }
+
+    /**
+     * String, int, Integer --> @RequestParam
+     * 나머지 --> @ModelAttribute (argument resolver 로 지정해둔 타입 제외)
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "Ok";
     }
 }
